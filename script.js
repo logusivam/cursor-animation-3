f0198979042932e3153bc426e65e386bd7354649
Loganathan G P <logusivam26@gmail.com>
2024-07-10 09:17:42 +0000

    Jul 10, 2024, 2:47 PM

--- script.js
+++ script.js
@@ -3,5 +3,21 @@ let clientY = -100;
 const innercursor = document.querySelector(".cursor--small");
 
 const initCursor = () => {
-    
-}
+    document.addEventListener("mousemove", e => {
+        clientX = e.clientX;
+        clientY = e.clientY;
+    });
+
+    const render = () => {
+        innercursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+        requestAnimationFrame(render);
+    };
+    requestAnimationFrame(render);
+};
+
+initCursor();
+let lastX = 0;
+let lastY = 0;
+let isStuck = false;
+let showCursor = false;
+let group, stuckX, stuckY, fillOuterCircle;
