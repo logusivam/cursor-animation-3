f2127456215cafd7d6c0bbdac450b94ed1155a6d
Loganathan G P <logusivam26@gmail.com>
2024-07-11 05:12:22 +0000

    Jul 11, 2024, 10:42 AM

--- script.js
+++ script.js
@@ -20,5 +20,21 @@ let lastX = 0;
 let lastY = 0;
 let isStuck = false;
 let showCursor = false;
-let group, stuckX, stuckY, fillOuterCircle;
+let group, stuckX, stuckY, fillOuterCursor;
 
+const initCanvas = () => {
+    const canvas = document.querySelector(".cursor--canvas");
+    const shapeBounds = {
+        width: 75,
+        height: 75
+    };
+    paper.setup(canvas);
+    const StrokeColor = "rgba(255, 0, 0, 0.5)";
+    const strokeWidth = 1;
+    const segments = 8;
+    const radius = 25;
+
+    const noiseScale = 150; //speed
+    const noiseRange = 4;  //range of distortion
+    let isNoisy = false; // state
+}
