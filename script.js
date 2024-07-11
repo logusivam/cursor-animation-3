b47cbf39c539cc9ea25e39d61a8b901d1c79a64e
Loganathan G P <logusivam26@gmail.com>
2024-07-11 05:58:13 +0000

    Jul 11, 2024, 11:28 AM

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+
 let clientX = -100;
 let clientY = -100;
 const innerCursor = document.querySelector(".cursor--small");
 
 const initCursor = () => {
-    document.addEventListener("mousemove", e => {
-        clientX = e.clientX;
-        clientY = e.clientY;
-    });
 
-    const render = () => {
-        innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
-        requestAnimationFrame(render);
-    };
+  document.addEventListener("mousemove", e => {
+    clientX = e.clientX;
+    clientY = e.clientY;
+  });
+  
+  
+  const render = () => {
+    innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+    
     requestAnimationFrame(render);
+  };
+  requestAnimationFrame(render);
 };
 
 initCursor();
@@ -23,74 +27,74 @@ let showCursor = false;
 let group, stuckX, stuckY, fillOuterCursor;
 
 const initCanvas = () => {
-    const canvas = document.querySelector(".cursor--canvas");
-    const shapeBounds = {
-        width: 75,
-        height: 75
-    };
-    paper.setup(canvas);
-    const StrokeColor = "rgba(255, 0, 0, 0.5)";
-    const strokeWidth = 1;
-    const segments = 8;
-    const radius = 25;
-
-    const noiseScale = 150; //speed
-    const noiseRange = 4;  //range of distortion
-    let isNoisy = false; // state
-
-    const polygon = new paper.Path.RegularPolygon(
-        new paper.Point(0, 0),
-        segments,
-        radius
+  const canvas = document.querySelector(".cursor--canvas");
+  const shapeBounds = {
+    width: 75,
+    height: 75
+  };
+  paper.setup(canvas);
+  const strokeColor = "rgba(255, 0, 0, 0.5)";
+  const strokeWidth = 1;
+  const segments = 8;
+  const radius = 25;
+  
+  const noiseScale = 150; // speed
+  const noiseRange = 4; // range of distortion
+  let isNoisy = false; // state
+  
+  const polygon = new paper.Path.RegularPolygon(
+    new paper.Point(0, 0),
+    segments,
+    radius
+  );
+  polygon.strokeColor = strokeColor;
+  polygon.strokeWidth = strokeWidth;
+  polygon.smooth();
+  group = new paper.Group([polygon]);
+  group.applyMatrix = false;
+  
+  const noiseObjects = polygon.segments.map(() => new SimplexNoise());
+  let bigCoordinates = [];
+  
+  
+  const lerp = (a, b, n) => {
+    return (1 - n) * a + n * b;
+  };
+  const map = (value, in_min, in_max, out_min, out_max) => {
+    return (
+      ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
     );
-    polygon.StrokeColor = StrokeColor;
-    polygon.strokeWidth = strokeWidth;
-    polygon.smooth();
-    group = new paper.Group([polygon]);
-    group.applyMatrix = false;
-
-    const noiseObjects = polygon.segments.map(() => 
-    new simplexNoise());
-    let bigCoordinates = [];
-
-    const lerp = (a, b, n) => {
-        return(1-n) * a + n * b;
-    };
-    const map = (value, in_min, in_max, out_min, out_max) => {
-        return (
-            ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-        );
-    };
-
-    paper.view.onFrame = event => {
-
-        lastX = lerp(lastX, clientX, 0.2);
-        lastY = lerp(lastY, clientY, 0.2);
-        group.position = new paper.Point(lastX, lastY);
-    }
+  };
+  
+  paper.view.onFrame = event => {
+   
+    lastX = lerp(lastX, clientX, 0.2);
+    lastY = lerp(lastY, clientY, 0.2);
+    group.position = new paper.Point(lastX, lastY);
+  }
 }
 
 initCanvas();
 const initHovers = () => {
-    
-    const handleMouseEnter = e => {
-        const navItem = e.currentTarget;
-        const navItemBox = navItem.getBoundingClientRect();
-        stuckX = Math.round(navItemBox.left + navItemBox.width / 2);
-        stuckY = Math.round(navItemBox.top + navItemBox.height / 2);
-        isStuck = true;
-    };
 
-    const handleMouseLeave = () => {
-        isStuck = false;
-    };
-
-    //Event listeners
-    const linkItems = document.querySelectorAll(".link");
-    linkItems.forEach(item => {
-        item.addEventListener("mouseenter", handleMouseEnter);
-        item.addEventListener("mouseleave", handleMouseLeave);
-    });
+  const handleMouseEnter = e => {
+    const navItem = e.currentTarget;
+    const navItemBox = navItem.getBoundingClientRect();
+    stuckX = Math.round(navItemBox.left + navItemBox.width / 2);
+    stuckY = Math.round(navItemBox.top + navItemBox.height / 2);
+    isStuck = true;
+  };
+  
+  const handleMouseLeave = () => {
+    isStuck = false;
+  };
+  
+  //Event listeners
+  const linkItems = document.querySelectorAll(".link");
+  linkItems.forEach(item => {
+    item.addEventListener("mouseenter", handleMouseEnter);
+    item.addEventListener("mouseleave", handleMouseLeave);
+  });
 };
 
 initHovers();
